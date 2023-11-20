130e7a7a7c6174deb2b0613c87542b6ec59a3ab9
Damian Białowąs <bialowas.damian@gmail.com>
2023-11-20 05:44:31 +0100

    added second section to html

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 alert("WITAJ");
 console.log("siema");
 console.log("siema");
+const firstName = "Damian";
+const age = 44;
+const welcome = document.querySelector(".navigation__div-js");
+welcome.innerHTML = `Nazywam się ${firstName} i mam ${age} lat`;
